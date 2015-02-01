b04718307c767e483b92e98bc77d0bc1a2efa284
Matt Murton <matt@rancon.co.uk>
2015-02-01 15:58:19 +0000

    Added EventRelay and EventRelayTests. All tests passing.

---
+++ test/robotlegs/bender/extensions/eventCommandMap/support/SupportEvent.cs
@@ -0,0 +1,27 @@
+﻿using robotlegs.bender.extensions.eventDispatcher.api;
+using System;
+
+
+namespace robotlegs.bender.extensions.eventCommandMap.support
+{
+	public class SupportEvent : IEvent
+	{
+		public enum Type
+		{
+			TYPE1,
+			TYPE2
+		}
+		private SupportEvent.Type _type;
+
+		public SupportEvent (SupportEvent.Type type)
+		{
+			_type = type;
+		}
+
+		public Enum type
+		{
+			get { return _type; }
+		}
+	}
+}
+
