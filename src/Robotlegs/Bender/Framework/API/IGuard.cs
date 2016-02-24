0081e7a5d8f13940a1288d7603a7d62a0a1b46a1
James Prankard <prankard@gmail.com>
2016-02-24 17:33:23 +0000

    Updated Copyright year and removed old Copyright on files

--- src/Robotlegs/Bender/Framework/API/IGuard.cs
+++ src/Robotlegs/Bender/Framework/API/IGuard.cs
@@ -1,5 +1,5 @@
 ﻿//------------------------------------------------------------------------------
-//  Copyright (c) 2014-2015 the original author or authors. All Rights Reserved. 
+//  Copyright (c) 2014-2016 the original author or authors. All Rights Reserved. 
 // 
 //  NOTICE: You are permitted to use, modify, and distribute this file 
 //  in accordance with the terms of the license agreement accompanying it. 
--- test/Robotlegs/Bender/Extensions/Matching/Support/TypeB12.cs
+++ test/Robotlegs/Bender/Extensions/Matching/Support/TypeB12.cs
@@ -1,12 +1,5 @@
 //------------------------------------------------------------------------------
-//  Copyright (c) 2014-2015 the original author or authors. All Rights Reserved. 
-// 
-//  NOTICE: You are permitted to use, modify, and distribute this file 
-//  in accordance with the terms of the license agreement accompanying it. 
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-//  Copyright (c) 2011 the original author or authors. All Rights Reserved. 
+//  Copyright (c) 2014-2016 the original author or authors. All Rights Reserved. 
 // 
 //  NOTICE: You are permitted to use, modify, and distribute this file 
 //  in accordance with the terms of the license agreement accompanying it. 
