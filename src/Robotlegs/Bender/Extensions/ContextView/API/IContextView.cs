f41e65dd324bd03d5d8b1675088bca434b8ab6b6
James Prankard <prankard@gmail.com>
2016-02-24 16:54:35 +0000

    More folder renaming

---
+++ src/Robotlegs/Bender/Extensions/ContextView/API/IContextView.cs
@@ -0,0 +1,15 @@
+//------------------------------------------------------------------------------
+//  Copyright (c) 2014-2015 the original author or authors. All Rights Reserved. 
+// 
+//  NOTICE: You are permitted to use, modify, and distribute this file 
+//  in accordance with the terms of the license agreement accompanying it. 
+//------------------------------------------------------------------------------
+
+namespace Robotlegs.Bender.Extensions.ContextView.API
+{
+	public interface IContextView
+	{
+		object view { get; }
+	}
+}
+
