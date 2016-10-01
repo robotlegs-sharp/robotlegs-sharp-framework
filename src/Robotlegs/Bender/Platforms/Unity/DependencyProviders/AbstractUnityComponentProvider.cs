ebf88aa7dd69da23e502da1c96734294c1b0e696
James Prankard <prankard@gmail.com>
2016-10-02 09:01:18 +1300

    Added singleton provider for Unity component Fixed pre-initialization error throwing with UnityComponentProvider

---
+++ src/Robotlegs/Bender/Platforms/Unity/DependencyProviders/AbstractUnityComponentProvider.cs
@@ -0,0 +1,101 @@
+﻿using Robotlegs.Bender.Extensions.ContextViews.API;
+using swiftsuspenders;
+using swiftsuspenders.dependencyproviders;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Robotlegs.Bender.DependencyProviders
+{
+	public abstract class AbstractUnityComponentProvider
+	{	
+		public event Action<DependencyProvider, object> PostApply
+		{
+			add
+			{
+				_postApply += value;
+			}
+			remove
+			{
+				_postApply -= value;
+			}
+		}
+
+		public event Action<DependencyProvider, object> PreDestroy
+		{
+			add
+			{
+				_preDestroy += value;
+			}
+			remove
+			{
+				_preDestroy -= value;
+			}
+		}
+
+		protected Type componentType;
+
+		protected Transform parentObject;
+
+		protected Component component;
+
+		protected Action<DependencyProvider, object> _postApply;
+
+		protected Action<DependencyProvider, object> _preDestroy;
+
+		private bool destroyGameObjectWhenComplete;
+
+		public AbstractUnityComponentProvider(Type componentType) : this(componentType, (Transform) null) { }
+
+		public AbstractUnityComponentProvider(Type componentType, GameObject parentObject) : this(componentType, parentObject.transform) { }
+
+		public AbstractUnityComponentProvider(Type componentType, Transform parentObject)
+		{
+			if (!componentType.IsSubclassOf(typeof(Component)))
+				throw (new Exception(string.Format("The componentType {0} must be a Unity Component", componentType)));
+			else
+			{
+				this.componentType = componentType;
+				this.parentObject = parentObject == null ? null : parentObject.transform;
+			}
+		}
+
+		public Component AddComponent(Injector activeInjector, Type componentType)
+		{
+			if (parentObject == null)
+			{
+				if (activeInjector.HasMapping(typeof(IContextView)))
+				{
+					IContextView contextViewObj = (IContextView) activeInjector.GetInstance(typeof(IContextView));
+					object contextView = contextViewObj.view;
+					if(contextView != null)
+					{
+						if (contextView is Transform) parentObject = (Transform) contextView;
+						else if (contextView is GameObject) parentObject = ((GameObject) contextView).transform;
+					}
+				}
+			}
+			if (parentObject == null)
+			{
+				destroyGameObjectWhenComplete = true;
+				parentObject = new GameObject("Unity Component Provider").transform;
+			}
+			component = parentObject.gameObject.AddComponent(componentType);
+			return component;
+		}
+
+		public void RemoveComponent()
+		{
+			if (destroyGameObjectWhenComplete && parentObject != null)
+			{
+				GameObject.Destroy(parentObject.gameObject);
+			}
+			else
+			{
+				Component.Destroy(component);
+			}
+			parentObject = null;
+			component = null;
+		}
+	}
+}
---
+++ src/Robotlegs/Bender/Platforms/Unity/DependencyProviders/UnitySingletonComponentProvider.cs
@@ -0,0 +1,51 @@
+﻿using Robotlegs.Bender.Extensions.ContextViews.API;
+using swiftsuspenders;
+using swiftsuspenders.dependencyproviders;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using swiftsuspenders.errors;
+
+namespace Robotlegs.Bender.DependencyProviders
+{
+	public class UnitySingletonComponentProvider : AbstractUnityComponentProvider, DependencyProvider
+	{
+		public UnitySingletonComponentProvider(Type componentType) : base(componentType) { }
+
+		public UnitySingletonComponentProvider(Type componentType, GameObject parentObject) : base(componentType, parentObject) { }
+
+		public UnitySingletonComponentProvider(Type componentType, Transform parentObject) : base(componentType, parentObject) { }
+
+		private bool _destroyed = false;
+
+		public object Apply(Type targetType, Injector activeInjector, Dictionary<string, object> injectParameters)
+		{
+			if (_destroyed)
+				throw new InjectorException("Forbidden usage of unmapped singleton provider for type "
+					+ targetType.ToString());
+
+			if (component == null)
+			{
+				AddComponent(activeInjector, componentType);
+
+				activeInjector.InjectInto(component);
+			}
+
+			if (_postApply != null)
+			{
+				_postApply(this, component);
+			}
+			return component;
+		}
+
+		public void Destroy()
+		{
+			if (_preDestroy != null)
+			{
+				_preDestroy(this, component);
+			}
+			_destroyed = true;
+			RemoveComponent();
+		}
+	}
+}
